32bf6d7cecbe173d92eaa11444e5d7a2ab226f3d
jominjose14 <42197279+jominjose14@users.noreply.github.com>
2020-06-07 18:24:08 +0530

    added feature to change coctrols

--- controls.js
+++ controls.js
@@ -1,29 +1,108 @@
+//2 control schemes
 
-//maintenance of control-key-bindings via the controls object
-var controls = {sticks: "e",
-                closedhh1: 'f',
-                closedhh2: 'd',
-                openhh: 'r',
-                snare1: 'g',
-                snare2: 'h',
-                rimshot: 'j',
-                bass1: 'b',
-                bass2: 'v',
-                lefttom: 't',
-                righttom: 'y',
-                floortom: 'u',
-                crash: 'm',
-                crashnbass: 'n',
-                ride: 'c'};
+const proControls = {
+  sticks: 'e',
+  closedhh1: 'f',
+  closedhh2: 'd',
+  openhh: 'r',
+  snare1: 'g',
+  snare2: 'h',
+  rimshot: 'j',
+  bass1: 'b',
+  bass2: 'v',
+  lefttom: 't',
+  righttom: 'y',
+  floortom: 'u',
+  crash: 'm',
+  crashnbass: 'n',
+  ride: 'c',
+};
 
-//function to update key-bindings in controls object
-function updateControls() {
-  var items = document.querySelectorAll(".item");
-  for(var i=0;i<items.length;i++) {
-    controls[items[i].id] = items[i].value;
-  }
-  alert("Controls Updated!");
+const easyControls = {
+  sticks: 'q',
+  closedhh1: '1',
+  closedhh2: 'w',
+  openhh: '2',
+  snare1: '3',
+  snare2: 'e',
+  rimshot: '4',
+  bass1: '5',
+  bass2: 'r',
+  lefttom: '8',
+  righttom: '9',
+  floortom: '0',
+  crash: '6',
+  crashnbass: 't',
+  ride: '7',
+};
+
+//setting up the controls object
+
+var controls = {};
+
+if (localStorage.getItem('controls') === null) {
+  controls = {
+    sticks: 'e',
+    closedhh1: 'f',
+    closedhh2: 'd',
+    openhh: 'r',
+    snare1: 'g',
+    snare2: 'h',
+    rimshot: 'j',
+    bass1: 'b',
+    bass2: 'v',
+    lefttom: 't',
+    righttom: 'y',
+    floortom: 'u',
+    crash: 'm',
+    crashnbass: 'n',
+    ride: 'c',
+  };
+  localStorage.setItem('controls', JSON.stringify(controls));
+} else {
+  controls = JSON.parse(localStorage.getItem('controls'));
 }
 
-//exports
-//export { controls };
+//---------------------Utility functions-------------------
+
+//function to update controls displayed on page
+function updateDisplayedControls() {
+  var items = document.querySelectorAll('.item');
+  for (var i = 0; i < items.length; i++) {
+    items[i].value = controls[items[i].id];
+  }
+}
+
+//function to save custom user controls
+function saveCustomControls() {
+  var items = document.querySelectorAll('.item');
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].value === null || items[i].value.length !== 1) {
+      alert(
+        'Error: Invalid control set. A valid drum control must be set to any 1 keyboard key'
+      );
+      return;
+    }
+    controls[items[i].id] = items[i].value;
+  }
+  localStorage.setItem('controls', JSON.stringify(controls));
+  alert('Custom Control Scheme updated successfully');
+}
+
+//function to choose easy controls scheme
+function setEasyControls() {
+  controls = easyControls;
+  localStorage.setItem('controls', JSON.stringify(controls));
+  updateDisplayedControls();
+  alert('Easy Control Scheme chosen successfully');
+}
+
+//function to choose pro controls scheme
+function setProControls() {
+  controls = proControls;
+  localStorage.setItem('controls', JSON.stringify(controls));
+  updateDisplayedControls();
+  alert('Pro Control Scheme chosen successfully');
+}
+
+updateDisplayedControls();
--- index.js
+++ index.js
@@ -1,252 +1,248 @@
-//-------------------------------Imports-------------------------------
+//Fetching controls
+var controls = {};
 
-//import { controls } from "controls.js";
-
-//maintenance of control-key-bindings via the controls object
-var controls = {sticks: "e",
-                closedhh1: 'f',
-                closedhh2: 'd',
-                openhh: 'r',
-                snare1: 'g',
-                snare2: 'h',
-                rimshot: 'j',
-                bass1: 'b',
-                bass2: 'v',
-                lefttom: 't',
-                righttom: 'y',
-                floortom: 'u',
-                crash: 'm',
-                crashnbass: 'n',
-                ride: 'c'};
+if (localStorage.getItem('controls') === null) {
+  controls = {
+    sticks: 'q',
+    closedhh1: '1',
+    closedhh2: 'w',
+    openhh: '2',
+    snare1: '3',
+    snare2: 'e',
+    rimshot: '4',
+    bass1: '5',
+    bass2: 'r',
+    lefttom: '8',
+    righttom: '9',
+    floortom: '0',
+    crash: '6',
+    crashnbass: 't',
+    ride: '7',
+  };
+  localStorage.setItem('controls', JSON.stringify(controls));
+} else {
+  controls = JSON.parse(localStorage.getItem('controls'));
+}
 
 //-------------------------Uitility Functions---------------------------
 
-//function to update buttons on DOM with current controls
-// function updateButtons() {
-//   for(var i=0;i<buttonArray.length;i++) {
-//     //filling innerHTML of each button with corresponding control key
-//     buttonArray[i].innerHTML = controls[buttonArray[i].id];
-//   }
-// }
-
 //setting up array of buttons
-var buttonArray = document.querySelectorAll(".drum");
+var buttonArray = document.querySelectorAll('.drum');
 
 //blink-animation
 function blink(button) {
-  button.classList.add("clicked");
-  setTimeout(function(){button.classList.remove("clicked");},100);
+  button.classList.add('clicked');
+  setTimeout(function () {
+    button.classList.remove('clicked');
+  }, 100);
 }
 
 //play an audio
 function playAudio(fileName) {
-  var audio = new Audio("./sounds/"+fileName+".mp3");
+  var audio = new Audio('./sounds/' + fileName + '.mp3');
   audio.play();
 }
 
 //-------------------DOM setup and Button Listeners for clicks------------------
 
-for(var i=0;i<buttonArray.length;i++) {
-
+for (var i = 0; i < buttonArray.length; i++) {
   //filling innerHTML of each button with corresponding control key
   buttonArray[i].innerHTML = controls[buttonArray[i].id];
 
   //click-listeners for all buttons
-  buttonArray[i].addEventListener("click",function() {
+  buttonArray[i].addEventListener('click', function () {
     //get clicked button's reference
     var clickedButton = this;
     //play audio
-    if(clickedButton.id === "crashnbass") {
-      playAudio("crash");
-      playAudio("bass1");
-    }
-    else {
+    if (clickedButton.id === 'crashnbass') {
+      playAudio('crash');
+      playAudio('bass1');
+    } else {
       playAudio(clickedButton.id);
     }
     //blink the button
     blink(clickedButton);
   });
-
 }
 
 //-----------------------Document Listeners for keypresses----------------------
 
 //sticks listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.sticks) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.sticks) {
     //play audio
-    playAudio("sticks");
+    playAudio('sticks');
     //get button corresponding to the key pressed
-    var button = document.getElementById("sticks");
+    var button = document.getElementById('sticks');
     //blink the button
     blink(button);
   }
 });
 
 //closedhh1 listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.closedhh1) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.closedhh1) {
     //play audio
-    playAudio("closedhh1");
+    playAudio('closedhh1');
     //get button corresponding to the key pressed
-    var button = document.getElementById("closedhh1");
+    var button = document.getElementById('closedhh1');
     //blink the button
     blink(button);
   }
 });
 
 //closedhh2 listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.closedhh2) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.closedhh2) {
     //play audio
-    playAudio("closedhh2");
+    playAudio('closedhh2');
     //get button corresponding to the key pressed
-    var button = document.getElementById("closedhh2");
+    var button = document.getElementById('closedhh2');
     //blink the button
     blink(button);
   }
 });
 
 //openhh listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.openhh) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.openhh) {
     //play audio
-    playAudio("openhh");
+    playAudio('openhh');
     //get button corresponding to the key pressed
-    var button = document.getElementById("openhh");
+    var button = document.getElementById('openhh');
     //blink the button
     blink(button);
   }
 });
 
 //snare1 listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.snare1) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.snare1) {
     //play audio
-    playAudio("snare1");
+    playAudio('snare1');
     //get button corresponding to the key pressed
-    var button = document.getElementById("snare1");
+    var button = document.getElementById('snare1');
     //blink the button
     blink(button);
   }
 });
 
 //snare2 listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.snare2) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.snare2) {
     //play audio
-    playAudio("snare2");
+    playAudio('snare2');
     //get button corresponding to the key pressed
-    var button = document.getElementById("snare2");
+    var button = document.getElementById('snare2');
     //blink the button
     blink(button);
   }
 });
 
 //rimshot listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.rimshot) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.rimshot) {
     //play audio
-    playAudio("rimshot");
+    playAudio('rimshot');
     //get button corresponding to the key pressed
-    var button = document.getElementById("rimshot");
+    var button = document.getElementById('rimshot');
     //blink the button
     blink(button);
   }
 });
 
 //bass1 listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.bass1) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.bass1) {
     //play audio
-    playAudio("bass1");
+    playAudio('bass1');
     //get button corresponding to the key pressed
-    var button = document.getElementById("bass1");
+    var button = document.getElementById('bass1');
     //blink the button
     blink(button);
   }
 });
 
 //bass2 listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.bass2) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.bass2) {
     //play audio
-    playAudio("bass2");
+    playAudio('bass2');
     //get button corresponding to the key pressed
-    var button = document.getElementById("bass2");
+    var button = document.getElementById('bass2');
     //blink the button
     blink(button);
   }
 });
 
 //lefttom listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.lefttom) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.lefttom) {
     //play audio
-    playAudio("lefttom");
+    playAudio('lefttom');
     //get button corresponding to the key pressed
-    var button = document.getElementById("lefttom");
+    var button = document.getElementById('lefttom');
     //blink the button
     blink(button);
   }
 });
 
 //righttom listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.righttom) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.righttom) {
     //play audio
-    playAudio("righttom");
+    playAudio('righttom');
     //get button corresponding to the key pressed
-    var button = document.getElementById("righttom");
+    var button = document.getElementById('righttom');
     //blink the button
     blink(button);
   }
 });
 
 //floortom listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.floortom) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.floortom) {
     //play audio
-    playAudio("floortom");
+    playAudio('floortom');
     //get button corresponding to the key pressed
-    var button = document.getElementById("floortom");
+    var button = document.getElementById('floortom');
     //blink the button
     blink(button);
   }
 });
 
 //crash listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.crash) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.crash) {
     //play audio
-    playAudio("crash");
+    playAudio('crash');
     //get button corresponding to the key pressed
-    var button = document.getElementById("crash");
+    var button = document.getElementById('crash');
     //blink the button
     blink(button);
   }
 });
 
 //crashnbass listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.crashnbass) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.crashnbass) {
     //play audio
-    playAudio("crash");
-    playAudio("bass1");
+    playAudio('crash');
+    playAudio('bass1');
     //get button corresponding to the key pressed
-    var button = document.getElementById("crashnbass");
+    var button = document.getElementById('crashnbass');
     //blink the button
     blink(button);
   }
 });
 
 //ride listener
-document.addEventListener("keypress",function(e) {
-  if(e.key === controls.ride) {
+document.addEventListener('keypress', function (e) {
+  if (e.key === controls.ride) {
     //play audio
-    playAudio("ride");
+    playAudio('ride');
     //get button corresponding to the key pressed
-    var button = document.getElementById("ride");
+    var button = document.getElementById('ride');
     //blink the button
     blink(button);
   }
